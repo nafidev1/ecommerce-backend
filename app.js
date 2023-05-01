f009b1b736b2c2a747f1951255836fdf4997bcb4
NAFI <nafidevtest@gmail.com>
2023-05-01 22:30:09 +0800

    revert back on cors

--- app.js
+++ app.js
@@ -21,13 +21,14 @@ const corsOptions = {
   origin: `${process.env.FRONTEND_LINK}`,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
-  exposedHeaders: 'Set-Cookie',
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Content-Type',
-    'Authorization'
-  ]
+  Headers: true
+  // exposedHeaders: 'Set-Cookie',
+  // methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: [
+    // 'Access-Control-Allow-Origin',
+    // 'Content-Type',
+    // 'Authorization'
+  // ]
 };
 app.use(cors(corsOptions));
 
