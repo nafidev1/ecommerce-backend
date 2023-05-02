d2dbb1087d4fb0715d2a0c83a867eb774b47622c
NAFI <nafidevtest@gmail.com>
2023-05-02 13:22:06 +0800

    revert back

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,7 @@ module.exports.signup = async (req, res, next) => {
       .cookie(
         "access_token",
         token,
-        { httpOnly: true, sameSite: "strict", secure: true },
+        { httpOnly: true, sameSite: "none", secure: true },
         {
           expires: new Date(Date.now() + 25892000000), // set expiry of 1month
         }
@@ -52,7 +52,7 @@ module.exports.login = async (req, res, next) => {
       .cookie(
         "access_token",
         token,
-        { httpOnly: true, sameSite: "strict", secure: true },
+        { httpOnly: true, sameSite: "none", secure: true },
         {
           expires: new Date(Date.now() + 25892000000), // set expiry of 1month
         }
