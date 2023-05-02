b1dea97c68444e3b7bab2f447b9d53359d4b8a39
NAFI <nafidevtest@gmail.com>
2023-05-02 13:01:44 +0800

    sameSite is strict

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,7 @@ module.exports.signup = async (req, res, next) => {
       .cookie(
         "access_token",
         token,
-        { httpOnly: true, sameSite: 'none', secure: true },
+        { httpOnly: true, sameSite: "strict" },
         {
           expires: new Date(Date.now() + 25892000000), // set expiry of 1month
         }
@@ -52,7 +52,7 @@ module.exports.login = async (req, res, next) => {
       .cookie(
         "access_token",
         token,
-        { httpOnly: true, sameSite: 'none', secure: true },
+        { httpOnly: true, sameSite: "strict" },
         {
           expires: new Date(Date.now() + 25892000000), // set expiry of 1month
         }
